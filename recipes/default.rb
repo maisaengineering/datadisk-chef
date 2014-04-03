310d729c0988810362c24426fe4892edc35875c9
Milo de Vries <milo@xdh.nl>
2014-04-03 19:13:22 +0200

    Refactor & failsafe

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,16 @@
 
 data_part = node['datadisk']['datapart']
 
+# nope out if #{data_part} already exists
+
+return if ::File.directory?("#{data_part}")
+
 # target disk enumeration
 
 disks_with_fs = node[:filesystem].to_s.scan(/[sx]v?d[a-z]/)
 disks = node[:block_device].to_s.scan(/[sx]v?d[a-z]/)
 disk_without_fs = disks - disks_with_fs
+target_part = "/dev/" + disk_without_fs.join + "1"
 
 # TODO: handle >1 disk without FS
 # TODO: handle disk without partition
@@ -29,17 +34,8 @@ execute "mkdir #{data_part}" do
        not_if { ::File.directory?("#{data_part}") }
 end
 
-#add mount point to fstab
-
-ruby_block "add mountpoint to /etc/fstab" do
-    block do
-        file = Chef::Util::FileEdit.new("/etc/fstab")
-        file.insert_line_if_no_match("#{data_part}",
-                "/dev/#{disk_without_fs.join}1  #{data_part}    ext3    defaults,nofail    1 1"
-        )
-        file.write_file
-        end
-        only_if { ::File.directory?("#{data_part}") }
+mount node['datadisk']['datapart'] do
+	device target_part
+	action [:mount, :enable]
+	fstype "ext3"
 end
-
-execute "mount #{data_part}"
